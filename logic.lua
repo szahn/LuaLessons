731e91d0f4e9eccae12e218c48ccec993b4e2cb4
Stuart Zahn <stuart.zahn@gmail.com>
2014-06-03 22:55:49 -0700

    loops

--- logic.lua
+++ logic.lua
@@ -9,5 +9,11 @@ print(y > x)
 isNotEqual = x ~= y
 print( isNotEqual)
 
+--Simple for loop
+for i = 1, 10 do
+print(i)
+end
 
-
+for i = 5, 10 do
+print(i)
+end
--- number_guess.lua
+++ number_guess.lua
@@ -4,14 +4,22 @@ math.randomseed(os.time()) --initialize the random number generator with a seed
 math.random()
 
 rand_num = math.random(100)
-answer = io.read("*n") --read number from std input
 
-if answer < rand_num then
-	print "Too low"
-elseif answer > rand_num then
-	print "Too high"
-else
-	print "You got it"
+--Keep asking for a number until it matches the random number
+while answer ~= rand_num do
+
+	answer = io.read("*n") --read number from std input
+
+	if answer < rand_num then
+		print "Too low"
+	elseif answer > rand_num then
+		print "Too high"
+	else
+		break
+	end
+
+	print "Guess again"
+
 end
 
 print("Number was "..rand_num)
